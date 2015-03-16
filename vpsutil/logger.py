61fe785dd3f273ed6de2a9a29d89ecaec74caa22
Oliver Palmer <oliverpalmer@opalmer.com>
2015-03-16 18:29:31 -0400

    setting logging to DEBUG for now

--- vpsutil/logger.py
+++ vpsutil/logger.py
@@ -1,4 +1,6 @@
 import logging
 
-logging.basicConfig(format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 logger = logging.getLogger("vpsutil")
+logger.setLevel(logging.DEBUG)
