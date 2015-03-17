b9c4703f1a8f609e45121778de58fd89402a4f06
Oliver Palmer <oliverpalmer@opalmer.com>
2015-03-16 20:05:46 -0400

    adding search functions and ability to create a droplet

--- vpsutil/core.py
+++ vpsutil/core.py
@@ -1,12 +1,25 @@
+import random
 import subprocess
+import time
+from collections import namedtuple
 from configparser import NoOptionError
 from os.path import expanduser, isfile
 
+try:
+    from http.client import NOT_FOUND, UNPROCESSABLE_ENTITY
+except ImportError:
+    from httplib import NOT_FOUND, UNPROCESSABLE_ENTITY
+
 from requests import Session as _Session
 from requests.adapters import HTTPAdapter
+from requests.exceptions import HTTPError
 
 from vpsutil.config import Providers, config
 from vpsutil.logger import logger
+
+
+_PowerState = namedtuple("PowerStates", ("ON", "OFF", "RESET"))
+PowerState = _PowerState(ON="on", OFF="off", RESET="reset")
 
 
 class BaseAPI(_Session):
@@ -121,6 +134,40 @@ class DigitalOceanAPI(BaseAPI):
             Authorization="Bearer %s" % config.get(
                 Providers.DIGITAL_OCEAN, "token"))
 
+    def get_distribution(self, slug=None, regions=None):
+        if slug is None:
+            slug = config.get(Providers.DIGITAL_OCEAN, "default_image")
+
+        if regions is None:
+            regions = []
+
+        logger.info(
+            "Searching for distribution %s (regions: %s)", slug, regions)
+        response = self.get(self.URL + "/images", params={"distribution": True})
+        response.raise_for_status()
+        data = response.json()
+
+        for dist in data["images"]:
+            if not dist["public"]:
+                logger.debug("... %s not public", dist["slug"])
+                continue
+
+            # At least one of the region slugs which came from
+            # get_regions() must be present in the distribution
+            for region_slug in regions:
+                if region_slug in dist["regions"]:
+                    break
+                else:
+                    logger.debug(
+                        "... %s region not in %s", region_slug, dist["slug"])
+            else:
+                continue
+
+            if dist["slug"] == slug:
+                return dist
+        else:
+            raise ValueError("Failed to locate distribution")
+
     # TODO: support filtering on features too
     def get_regions(self, size, slug_prefix=None, features=None):
         """
@@ -190,6 +237,7 @@ class DigitalOceanAPI(BaseAPI):
 
             regions.append(region)
 
+        logger.debug("... regions: %r", [region["slug"] for region in regions])
         return regions
 
     def _get_ssh_fingerprint(self, path):
@@ -226,3 +274,130 @@ class DigitalOceanAPI(BaseAPI):
                 }
             )
             response.raise_for_status()
+
+    def create_host(
+            self, hostname, size, distribution=None, bootstrap=None):
+        features = []
+        if bootstrap:
+            features.append("metadata")
+
+        regions = self.get_regions(size, features=features)
+        region_slugs = [region["slug"] for region in regions]
+        distribution = self.get_distribution(
+            slug=distribution, regions=region_slugs)
+
+        # Get all regions which both get_regions() and get_distribution()
+        # agree on.
+        region_slug = random.choice(
+            list(set(distribution["regions"]) & set(region_slugs)))
+
+        logger.info("Creating %s @ %s in %s", hostname, size, region_slug)
+        data = {
+            "name": hostname,
+            "region": region_slug,
+            "size": size,
+            "image": distribution["id"]
+        }
+
+        if bootstrap and isfile(bootstrap):
+            bootstrap = open(bootstrap, "r").read()
+
+        if bootstrap:
+            data.update(user_data=bootstrap)
+
+        # Create the host
+        response = self.post(
+            self.URL + "/droplets",
+            data=data
+        )
+        response.raise_for_status()
+        return response.json()["droplet"]
+
+    def power(self, droplet_id, state):
+        logger.info("Set power state of droplet %d to %s", droplet_id, state)
+        if state is PowerState.ON:
+            response = self.post(
+                self.URL + "/droplets/%d/actions" % droplet_id,
+                data={"type": "power_on"}
+            )
+            response.raise_for_status()
+            return response.json()["action"]
+
+        raise NotImplementedError(state)
+
+    def destroy(self, droplet):
+        if isinstance(droplet, int):
+            try:
+                droplet = self.get_droplet(droplet)
+            except HTTPError as e:
+                if e.response.status_code == NOT_FOUND:
+                    logger.debug("... %d does not exist", droplet)
+                    return
+                raise
+
+        assert isinstance(droplet, dict)
+        logger.info("Destroy droplet %d", droplet["id"])
+
+        while True:
+            response = self.delete(self.URL + "/droplets/%d" % droplet["id"])
+            if response.status_code == UNPROCESSABLE_ENTITY:
+                time.sleep(5)
+                logger.debug("... retry")
+                continue
+
+            response.raise_for_status()
+            break
+
+    def get_droplet(self, droplet_id):
+        assert isinstance(droplet_id, int)
+        logger.info("Get droplet %d", droplet_id)
+        response = self.get(self.URL + "/droplets/%d" % droplet_id)
+        response.raise_for_status()
+        return response.json()["droplet"]
+
+    def get_public_ip(self, droplet, ip_version="v4"):
+        if isinstance(droplet, int):
+            # We might not have a network yet
+            while True:
+                result = self.get_droplet(droplet)
+                if result["networks"][ip_version]:
+                    droplet = result
+                    break
+                time.sleep(3)
+
+        logger.info("Get IP of droplet %s", droplet["id"])
+        addresses = []
+        for address in droplet["networks"][ip_version]:
+            if address["type"] == "public":
+                addresses.append(address["ip_address"])
+
+        assert len(addresses) == 1
+        return addresses[0]
+
+    def find_droplets(self, **fields):
+        response = self.get(self.URL + "/droplets")
+        response.raise_for_status()
+        data = response.json()
+
+        for droplet in data["droplets"]:
+            for key, value in fields.items():
+                if key not in ("ip", ):
+                    if key not in droplet:
+                        raise KeyError("No such key %s" % key)
+
+                    if droplet[key] != value:
+                        break
+
+                if key == "ip" and value != self.get_public_ip(droplet):
+                    break
+
+            else:
+                yield droplet
+
+    def find_droplet(self, **fields):
+        droplets = list(self.find_droplets(**fields))
+        if not droplets:
+            raise ValueError("No droplets found matching %s" % fields)
+        if len(droplets) > 1:
+            raise ValueError("Found multiple droplets matching %s" % fields)
+        return droplets[0]
